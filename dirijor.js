f6130938ff7970955c38932b9485213ecfad06eb
raduluca13 <radu.luca13@yahoo.com>
2018-10-30 16:34:38 +0200

    updated dirijor

--- dirijor.js
+++ dirijor.js
@@ -6,22 +6,26 @@ var wavesurfer1 = WaveSurfer.create({
     container: '#waveform1',
     waveColor: 'violet',
     progressColor: 'green',
-    crossOrigin: 'anonymous'
+    crossOrigin: 'anonymous',
+    autoCenter: 'true'
 });
 var wavesurfer2 = WaveSurfer.create({
     container: '#waveform2',
     waveColor: 'green',
-    progressColor: 'purple'
+    progressColor: 'purple',
+    autoCenter: 'true'
 });
 var wavesurfer3 = WaveSurfer.create({
     container: '#waveform3',
     waveColor: 'green',
-    progressColor: 'purple'
+    progressColor: 'purple',
+    autoCenter: 'true'
 });
 var wavesurfer4 = WaveSurfer.create({
     container: '#waveform4',
     waveColor: 'green',
-    progressColor: 'purple'
+    progressColor: 'purple',
+    autoCenter: 'true'
 });
 
 wavesurfer1.load("http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3");
@@ -33,22 +37,30 @@ var wavePartitura1 = WaveSurfer.create({
     container: '#partitura1',
     waveColor: 'violet',
     progressColor: 'green',
-    crossOrigin: 'anonymous'
+    crossOrigin: 'anonymous',
+    autoCenter: 'true',
+    barWidth: "6"
 });
 var wavePartitura2 = WaveSurfer.create({
     container: '#partitura2',
     waveColor: 'green',
-    progressColor: 'purple'
+    progressColor: 'purple',
+    autoCenter: 'true',
+    barWidth: "6"
 });
 var wavePartitura3 = WaveSurfer.create({
     container: '#partitura3',
     waveColor: 'green',
-    progressColor: 'purple'
+    progressColor: 'purple',
+    autoCenter: 'true',
+    barWidth: "6"
 });
 var wavePartitura4 = WaveSurfer.create({
     container: '#partitura4',
     waveColor: 'green',
-    progressColor: 'purple'
+    progressColor: 'purple',
+    autoCenter: 'true',
+    barWidth: "6"
 });
 
 wavePartitura1.load("http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3");
@@ -67,6 +79,8 @@ var themeOptionSelector = document.getElementById("themeSelector_select");
 //         iterations: Infinity
 //     }
 // );
+
+let progress = document.getElementById('progress');
 main.addEventListener("click", function(e){
     if (e.target.parentNode.id == "waveform1"){
         wavesurfer1.playPause();
