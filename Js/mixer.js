ab2d7ad5b4b1aa7d5a33be5a82d500e90324174e
Bleoju Ionut <ionutyop@gmail.com>
2018-12-06 22:48:11 +0200

    Updated mixer
    Added add functionality for multiple scores.

--- Js/mixer.js
+++ Js/mixer.js
@@ -10,4 +10,22 @@ function windowOnClick(event) {
             event.target.classList.add("active_cell");
         }
     }
+    if(event.target.classList.contains("fa-plus-square")){
+        let mainContainer = document.getElementsByClassName("container")[0];
+        let mixerTable = document.createElement("div");
+        var childsNumber = document.getElementById("mixer_container").childElementCount;
+        mixerTable.classList.add("mixer_table_part");
+        for(i = 0 ; i <8 ; i++){
+            let gridColumn = document.createElement("div");
+            gridColumn.classList.add("grid_column");
+            for(j = 0 ; j<16 ; j++){
+                let gridCell = document.createElement("div");
+                gridCell.classList.add("grid_cell");
+                gridColumn.appendChild(gridCell);
+            }
+            mixerTable.appendChild(gridColumn);
+        }
+        mainContainer.insertBefore(mixerTable,mainContainer.childNodes[childsNumber]);
+    }
   }
+
