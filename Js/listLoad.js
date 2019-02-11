5687b5a328947ff8c07d01ee601d9b04cc6c511f
ionutyop <ionutyop@gmail.com>
2019-02-11 21:43:28 +0200

    Added bpm + changeInstrument
    Added a small bpm slider + the ability to select songs based on the instrument.

--- Js/listLoad.js
+++ Js/listLoad.js
@@ -1,27 +1,46 @@
-var songList = document.getElementById("partituri_instrument");
+
+var optionList = document.getElementById("instrument_options");
 var db = firebase.firestore();
 var songsRef = db.collection("Songs");
 
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        const quary = songsRef;
-        quary.get()
-            .then(songs => {
-                let i = 5;
-                songs.forEach(doc => {
-                    var data = doc.data();
-                    var song = document.createElement("div");
-                    song.id = "partitura" + i;
-                    song.draggable = true;
-                    song.innerText = data.user + " - " + data.title;
-                    song.dataset.notes = data.notes
-                    song.dataset.instrumentName = "xylophone" // this is default
-                    song.classList.add("waveformDiv");
-                    song.classList.add("partitura");
-                    songList.appendChild(song);
-                    i++;
-                })
 
-            })
+function changeSongList(id){
+    var list = document.createElement("div");
+    list.id = "partituri_instrument";
+    if(document.getElementById("partituri_instrument")){
+    optionList.removeChild(document.getElementById("partituri_instrument"));
     }
-});
+    var instrumentName;
+    if(id==1){
+        instrumentName = "grand_piano";
+    }else if(id==2){
+        instrumentName = "xylophone";
+    }else{
+        instrumentName = "music_box";
+    }
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            const quary = songsRef.where("instrument","==",instrumentName);
+            quary.get()
+                .then(songs => {
+                    let i = 5;
+                    songs.forEach(doc => {
+                        var data = doc.data();
+                        var song = document.createElement("div");
+                        song.id = "partitura" + i;
+                        song.draggable = true;
+                        song.innerText = data.user + " - " + data.title;
+                        song.dataset.notes = data.notes;
+                        song.dataset.instrumentName = instrumentName;
+                        song.classList.add("waveformDiv");
+                        song.classList.add("partitura");
+                        list.appendChild(song);
+                        optionList.appendChild(list);
+                        i++;
+                    })
+    
+                })
+        }
+    });
+}
+changeSongList(1);
